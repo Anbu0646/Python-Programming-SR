a1db4531935f6c5e410678abf6d560b12e6d7b3e
Anbu S <SIT20EC045@SAIRAMTAP.EDU.IN>
2023-01-03 15:55:07 +0530

    Create Alternate letters in Uppercase.py

---
+++ Alternate letters in Uppercase.py
@@ -0,0 +1,28 @@
+'''
+A str‌ing S (only alphabets) is passed as input. The printed output should contain alphabets in odd positions in each word in uppercase and alphabets in even positions in each word in lowercase. 
+
+Input Format:  The first line will contain S. 
+Output Format: The first line will contain the resultant string value based on the conditions provided. 
+
+Boundary Conditions: Length of S is from 3 to 100. 
+
+Example Input/Output 1: 
+
+Input:  tREE GiVES us fruiTS 
+Output: TrEe GiVeS Us FrUiTs 
+
+Example Input/Output 2: 
+
+Input:  FLoweR iS beauTIFUL
+Output: FlOwEr Is BeAuTiFuL
+
+SOLUTION:
+'''
+
+s=list(input().split(" ")
+for i in range(0, len(s)):
+    l=list(s[i])
+    for j in range(0, len(l)):
+        if (j%2==0): print(l[j].upper(), end="")
+        else: print(l[j].lower(), end="")
+    print(" ",end="")
